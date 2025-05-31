79ff55fa7a10f66972c5f31a32bf45b5e8c22e93
Rashwan Mohamed <rashwanmohmed2@gmail.com>
2025-05-31 14:24:29 +0300

    'Guest Done'

--- kanban-front/src/features/auth/Logout.tsx
+++ kanban-front/src/features/auth/Logout.tsx
@@ -1,20 +1,23 @@
 import {client} from "@/main.tsx";
 import {loggedOut} from "@/features/auth/AuthenticationSlice.tsx";
 import {useNavigate} from "react-router-dom";
-import {useAppDispatch} from "@/app/hooks.ts";
+import {useAppDispatch, useAppSelector} from "@/app/hooks.ts";
 import {useMutation} from "@apollo/client";
-import {LOGOUT} from "@/GraphQL Queries/SessionQueries.ts";
+import {LOGOUT, LOGOUT_GUEST} from "@/GraphQL Queries/SessionQueries.ts";
 import LoadingSpinner from "@/generalComponents/loadingSpinner.tsx";
 import {setBoards} from "@/features/board/boardSlice.ts";
-import { UseAppContext} from "@/context.tsx";
+import {UseAppContext} from "@/context.tsx";
 
 const Logout = () => {
+    const {isGuest} = useAppSelector(state => state.auth)
     const [logout, {loading}] = useMutation(LOGOUT)
+    const [logoutGuest, {loading: loggingGuestOut}] = useMutation(LOGOUT_GUEST)
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
     const {setSelected} = UseAppContext()
+    const loggingOutFun = isGuest ? logoutGuest : logout
     const handleLogOut = async () => {
-        await logout(); // Inform backend
+        await loggingOutFun(); // Inform backend
         await client.clearStore();
         localStorage.removeItem('user');
         dispatch(loggedOut())
@@ -24,7 +27,8 @@ const Logout = () => {
         // or next tick console.log("Logged out completely.");
     }
 
-    if (loading) return <p className={'LoggingOutMessage'}><LoadingSpinner message={'Logging Out ...'}></LoadingSpinner>
+    if (loading || loggingGuestOut) return <p className={'LoggingOutMessage'}><LoadingSpinner
+        message={'Logging Out ...'}></LoadingSpinner>
     </p>
     return (
         <>
