69bb7deea2e5b9176916dfbd57d92d99d51a0641
Rashwan Mohamed <87570420+Rashwan-Mohamed@users.noreply.github.com>
2025-06-13 11:07:00 +0300

    update

--- Backend/src/GraphQL/Datasource/AuthDataSource.php
+++ Backend/src/GraphQL/Datasource/AuthDataSource.php
@@ -1,176 +1,179 @@
 <?php
 
-    namespace App\GraphQL\Datasource;
+namespace App\GraphQL\Datasource;
 
-    use App\GraphQL\Resolvers\Mutation\Auth;
-    use Exception;
-    use PDOException;
+use App\GraphQL\Resolvers\Mutation\Auth;
+use Exception;
+use PDOException;
 
 
-    class AuthDataSource extends BaseDataSource
+class AuthDataSource extends BaseDataSource
+{
+    protected string $REGISTER_NEW_USER = "INSERT  INTO kanban.users (username, email, password, created_at,isGuest) VALUES (:user, :email, :password, DEFAULT,:isGuest)";
+    protected string $GET_USER_ID = "SELECT id FROM kanban.users WHERE username = :user";
+    protected string $GET_LAST_GUEST_ADDED_USER = "SELECT * FROM kanban.users WHERE isGuest=1 ORDER BY created_at DESC LIMIT 1";
+    protected string $DELETE_USER = "DELETE FROM kanban.users WHERE id = :id";
+    protected string $DELETE_ALL_GUESTS_USERS = "DELETE FROM kanban.users WHERE isGuest = 1 AND guest_expired_at < NOW()";
+    protected string $REGISTER_GUEST = "INSERT INTO kanban.users (username, email, password, created_at,isGuest,guest_expired_at) VALUES (DEFAULT, DEFAULT, 123, DEFAULT,1,NOW() + INTERVAL 1 HOUR)";
+    protected string $CHANGE_DETAILS = "UPDATE kanban.users t SET t.username = :username, t.email = :email WHERE t.id = :userId";
+    protected string $CHANGE_PASSWORD = "UPDATE kanban.users t SET t.password = :password WHERE t.id = :userId";
+    protected string $GET_USER_BY_ID = "SELECT * FROM kanban.users WHERE id = :id";
+    protected string $CHANGE_GUEST_TO_USER = "UPDATE kanban.users t SET t.username = :user, t.email=:email,isGuest=0,password=:password WHERE t.id = :userId";
+
+    public function handleRegister($password, $username, $email, $guest = false, $userId = null)
     {
-        protected string $REGISTER_NEW_USER = "INSERT  INTO kanban.users (username, email, password, created_at,isGuest) VALUES (:user, :email, :password, DEFAULT,:isGuest)";
-        protected string $GET_USER_ID = "SELECT id FROM kanban.users WHERE username = :user";
-        protected string $GET_LAST_GUEST_ADDED_USER = "SELECT * FROM kanban.users WHERE isGuest=1 ORDER BY created_at DESC LIMIT 1";
-        protected string $DELETE_USER = "DELETE FROM kanban.users WHERE id = :id";
-        protected string $DELETE_ALL_GUESTS_USERS = "DELETE FROM kanban.users WHERE isGuest = 1 AND guest_expired_at < NOW()";
-        protected string $REGISTER_GUEST = "INSERT INTO kanban.users (username, email, password, created_at,isGuest,guest_expired_at) VALUES (DEFAULT, DEFAULT, 123, DEFAULT,1,NOW() + INTERVAL 1 HOUR)";
-        protected string $CHANGE_DETAILS = "UPDATE kanban.users t SET t.username = :username, t.email = :email WHERE t.id = :userId";
-        protected string $CHANGE_PASSWORD = "UPDATE kanban.users t SET t.password = :password WHERE t.id = :userId";
-        protected string $GET_USER_BY_ID = "SELECT * FROM kanban.users WHERE id = :id";
-        protected string $CHANGE_GUEST_TO_USER = "UPDATE kanban.users t SET t.username = :user, t.email=:email,isGuest=0,password=:password WHERE t.id = :userId";
-
-        public function handleRegister($password, $username, $email, $guest = false, $userId = null)
-        {
-            $userExists = $this->db->query("SELECT COUNT(*) as count FROM kanban.users WHERE username = :username", [':username' => $username])->find()['count'] > 0;
-            $emailExists = $this->db->query("SELECT COUNT(*) as count FROM kanban.users WHERE email = :email", [':email' => $email])->find()['count'] > 0;
-            if ($userExists || $emailExists) {
-                return ([
-                    'successful' => false,
-                    'userId' => null,
-                    'existingUser' => $userExists,
-                    'existingEmail' => $emailExists,
-
-                ]);
-            }
-
-            try {
-                if (!$userId) {
-                    $this->db->query($this->REGISTER_NEW_USER, [':user' => $username, ':email' => $email, ':password' => $password, ':isGuest' => $guest ? 1 : 0]);
-                    $id = $this->db->query($this->GET_USER_ID, [':user' => $username])->find();
-                    $id = $id['id'];
-                } else {
-                    $this->db->query($this->CHANGE_GUEST_TO_USER, [':user' => $username, ':email' => $email, ':password' => $password, ':userId' => $userId]);
-                    $id = $userId;
-                }
-            } catch (PDOException  $e) {
-                $code = $e->getCode();
-                if ($code == 23000) {
-                    $hasUsername = str_contains($username, 'username');
-                    $hasEmail = str_contains($username, 'email');
-                    if ($hasEmail || $hasUsername) {
-                        return ([
-                            'successful' => false,
-                            'userId' => null,
-                            'existingUser' => $hasUsername,
-                            'existingEmail' => $hasEmail,
-                        ]);
-                    }
-                }
-                return ([]);
-            }
-
-            $user = [
-                'id' => $id,
-                'username' => $username,
-                'email' => $email,
-                'isGuest' => $guest,
-            ];
-
-            Auth::newSession($user);
-            if (!isset($userId)) {
-                insertData();
-            }
+        $userExists = $this->db->query("SELECT COUNT(*) as count FROM kanban.users WHERE username = :username", [':username' => $username])->find()['count'] > 0;
+        $emailExists = $this->db->query("SELECT COUNT(*) as count FROM kanban.users WHERE email = :email", [':email' => $email])->find()['count'] > 0;
+        if ($userExists || $emailExists) {
             return ([
-                'successful' => true,
-                'userId' => $id,
+                'successful' => false,
+                'userId' => null,
+                'existingUser' => $userExists,
+                'existingEmail' => $emailExists,
+
             ]);
         }
 
-
-        public function handleLogin($plainPassword, $username)
-        {
-            $user = $this->db->query("SELECT * FROM kanban.users WHERE username = :username", [
-                ':username' => $username
-            ])->get();
-
-            if (!$user) {
-                return ['message' => 'USER_NOT_FOUND', 'validity' => null];
-            };
-
-            $user = $user[0];
-            if (!password_verify($plainPassword, $user['password'])) {
-                return ['message' => 'INVALID_PASSWORD', 'validity' => null];
+        try {
+            if (!$userId) {
+                $this->db->query($this->REGISTER_NEW_USER, [':user' => $username, ':email' => $email, ':password' => $password, ':isGuest' => $guest ? 1 : 0]);
+                $id = $this->db->query($this->GET_USER_ID, [':user' => $username])->find();
+                $id = $id['id'];
+            } else {
+                $this->db->query($this->CHANGE_GUEST_TO_USER, [':user' => $username, ':email' => $email, ':password' => $password, ':userId' => $userId]);
+                $id = $userId;
             }
-
-            return [
-                'message' => 'credentials are correct',
-                'validity' => true,
-                'user' => self::formatUser($user),
-            ];
+        } catch (PDOException  $e) {
+            $code = $e->getCode();
+            if ($code == 23000) {
+                $hasUsername = str_contains($username, 'username');
+                $hasEmail = str_contains($username, 'email');
+                if ($hasEmail || $hasUsername) {
+                    return ([
+                        'successful' => false,
+                        'userId' => null,
+                        'existingUser' => $hasUsername,
+                        'existingEmail' => $hasEmail,
+                    ]);
+                }
+            }
+            return ([]);
         }
 
-        public function handleDeleteUser($id)
-        {
-            $this->db->query($this->DELETE_USER, ['id' => $id]);
-            return true;
-        }
+        $user = [
+            'id' => $id,
+            'username' => $username,
+            'email' => $email,
+            'isGuest' => $guest,
+        ];
 
-        public function handleNewGuest()
-        {
-            $this->db->query($this->REGISTER_GUEST);
-            $user = $this->db->query($this->GET_LAST_GUEST_ADDED_USER)->find();
-            Auth::newSession($user);
+        Auth::newSession($user);
+        if (!isset($userId)) {
             insertData();
-            return $user['id'];
         }
-
-        public function cleanGuests()
-        {
-            $this->db->query($this->DELETE_ALL_GUESTS_USERS);
-            return "Old guest users deleted successfully at " . date('Y-m-d H:i:s') . "\n";
-        }
-
-        public function changeProfile($newName, $newEmail, $userId, $oldPassword, $newPassword)
-        {
-            $this->db->query($this->CHANGE_DETAILS, ['username' => $newName, 'email' => $newEmail, 'userId' => $userId]);
-            if ($newPassword) {
-                $newPassword = password_hash($newPassword, PASSWORD_DEFAULT);
-                $password = $this->db->query('SELECT password from kanban.users u WHERE u.id=:id ', ['id' => $userId])->find()['password'];
-                if (password_verify($oldPassword, $password)) {
-                    $this->db->query($this->CHANGE_PASSWORD, ['password' => $newPassword, 'userId' => $userId]);
-                    return ['message' => 'PROFILE_DETAILS_CHANGED', 'successful' => true];
-                } else {
-                    return ['message' => 'PASSWORD DOES NOT MATCH', 'successful' => false];
-                }
-            }
-            return ['message' => 'PROFILE_DETAILS_CHANGED', 'successful' => true];
-        }
-
-        /**
-         * @throws Exception
-         */
-        public function getUser($id)
-        {
-            try {
-                $result = $this->db->query($this->GET_USER_BY_ID, ['id' => $id])->get();
-                if (empty($result)) {
-                    return null; // No user found
-                }
-                return self::formatUser($result[0]);
-
-            } catch (PDOException $e) {
-                error_log("Database error: " . $e->getMessage()); // Log the error
-                return null; // Handle gracefully
-            }
-        }
-
-        public function markTourTaken($userId)
-        {
-            try {
-                $this->db->query("UPDATE kanban.users SET taken_tour = 1 WHERE id = :id", ['id' => $userId]);
-                return true;
-            } catch (PDOException $e) {
-                error_log("Failed to mark tour as taken: " . $e->getMessage());
-                return false;
-            }
-        }
-        protected static function formatUser($user): array
-        {
-            return ['id' => $user['id'],
-                'username' => $user['username'],
-                'email' => $user['email'], 'isGuest' => $user['isGuest'], 'last_updated' => $user['last_updated'],'created_at' => $user['created_at'],'takenTour'=>$user['taken_tour']];
-        }
-
-
+        return ([
+            'successful' => true,
+            'userId' => $id,
+        ]);
     }
+
+
+    public function handleLogin($plainPassword, $username)
+    {
+        $user = $this->db->query("SELECT * FROM kanban.users WHERE username = :username", [
+            ':username' => $username
+        ])->get();
+
+        if (!$user) {
+            return ['message' => 'USER_NOT_FOUND', 'validity' => null];
+        };
+
+        $user = $user[0];
+        if (!password_verify($plainPassword, $user['password'])) {
+            return ['message' => 'INVALID_PASSWORD', 'validity' => null];
+        }
+
+        return [
+            'message' => 'credentials are correct',
+            'validity' => true,
+            'user' => self::formatUser($user),
+        ];
+    }
+
+    public function handleDeleteUser($id)
+    {
+        $this->db->query($this->DELETE_USER, ['id' => $id]);
+        return true;
+    }
+
+    public function handleNewGuest()
+    {
+        $this->db->query($this->REGISTER_GUEST);
+        $user = $this->db->query($this->GET_LAST_GUEST_ADDED_USER)->find();
+        Auth::newSession($user);
+        insertData();
+        return $user['id'];
+    }
+
+    public function cleanGuests()
+    {
+        $this->db->query($this->DELETE_ALL_GUESTS_USERS);
+        return "Old guest users deleted successfully at " . date('Y-m-d H:i:s') . "\n";
+    }
+
+    public function changeProfile($newName, $newEmail, $userId, $oldPassword, $newPassword)
+    {
+        $this->db->query($this->CHANGE_DETAILS, ['username' => $newName, 'email' => $newEmail, 'userId' => $userId]);
+        if ($newPassword) {
+            $newPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+            $password = $this->db->query('SELECT password from kanban.users u WHERE u.id=:id ', ['id' => $userId])->find()['password'];
+            if (password_verify($oldPassword, $password)) {
+                $this->db->query($this->CHANGE_PASSWORD, ['password' => $newPassword, 'userId' => $userId]);
+                return ['message' => 'PROFILE_DETAILS_CHANGED', 'successful' => true];
+            } else {
+                return ['message' => 'PASSWORD DOES NOT MATCH', 'successful' => false];
+            }
+        }
+        return ['message' => 'PROFILE_DETAILS_CHANGED', 'successful' => true];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getUser($id)
+    {
+        try {
+            $result = $this->db->query($this->GET_USER_BY_ID, ['id' => $id])->get();
+            if (empty($result)) {
+                return null; // No user found
+            }
+            return self::formatUser($result[0]);
+        } catch (PDOException $e) {
+            error_log("Database error: " . $e->getMessage()); // Log the error
+            return null; // Handle gracefully
+        }
+    }
+
+    public function markTourTaken($userId)
+    {
+        try {
+            $this->db->query("UPDATE kanban.users SET taken_tour = 1 WHERE id = :id", ['id' => $userId]);
+            return true;
+        } catch (PDOException $e) {
+            error_log("Failed to mark tour as taken: " . $e->getMessage());
+            return false;
+        }
+    }
+    protected static function formatUser($user): array
+    {
+        return [
+            'id' => $user['id'],
+            'username' => $user['username'],
+            'email' => $user['email'],
+            'isGuest' => $user['isGuest'],
+            'last_updated' => $user['last_updated'],
+            'created_at' => $user['created_at'],
+            'takenTour' => $user['taken_tour']
+        ];
+    }
+}
